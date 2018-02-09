a95a2174de8c439e7247ba42189eec54ba954700
Michael Simacek <msimacek@redhat.com>
2018-02-09 22:56:12 +0100

    Propagate segment into executeFree

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -4351,6 +4351,7 @@ void Executor::executeAlloc(ExecutionState &state,
 }
 
 void Executor::executeFree(ExecutionState &state,
+                           ref<Expr> segment,
                            ref<Expr> address,
                            KInstruction *target) {
   address = optimizer.optimizeExpr(address, true);
