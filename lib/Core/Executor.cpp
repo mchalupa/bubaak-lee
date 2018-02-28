fef00cc196c7c15121d9f00749adaeb0b2be66a1
Michael Simacek <msimacek@redhat.com>
2018-02-28 22:07:44 +0100

    Use new shortcut methods

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -2133,8 +2133,7 @@ void Executor::executeInstruction(ExecutionState &state, KInstruction *ki) {
     KInstIterator kcaller = state.stack.back().caller;
     Instruction *caller = kcaller ? kcaller->inst : nullptr;
     bool isVoidReturn = (ri->getNumOperands() == 0);
-    KValue result(ConstantExpr::alloc(0, Expr::Bool),
-                  ConstantExpr::alloc(0, Expr::Bool));
+    KValue result(ConstantExpr::alloc(0, Expr::Bool));
     
     if (!isVoidReturn) {
       result = eval(ki, 0, state);
@@ -2205,11 +2204,9 @@ void Executor::executeInstruction(ExecutionState &state, KInstruction *ki) {
             // XXX need to check other param attrs ?
             bool isSExt = cb.hasRetAttr(llvm::Attribute::SExt);
             if (isSExt) {
-              result.set(SExtExpr::create(result.getSegment(), to),
-                         SExtExpr::create(result.getOffset(), to));
+              result = result.SExt(to);
             } else {
-              result.set(ZExtExpr::create(result.getSegment(), to),
-                         ZExtExpr::create(result.getOffset(), to));
+              result = result.ZExt(to);
             }
           }
 
@@ -2510,11 +2507,9 @@ void Executor::executeInstruction(ExecutionState &state, KInstruction *ki) {
               // XXX need to check other param attrs ?
               bool isSExt = cb.paramHasAttr(i, llvm::Attribute::SExt);
               if (isSExt) {
-                arguments[i].value = SExtExpr::create(arguments[i].value, to);
-                arguments[i].pointerSegment = SExtExpr::create(arguments[i].pointerSegment, to);
+                arguments[i] = arguments[i].SExt(to);
               } else {
-                arguments[i].value = ZExtExpr::create(arguments[i].value, to);
-                arguments[i].pointerSegment = ZExtExpr::create(arguments[i].pointerSegment, to);
+                arguments[i] = arguments[i].ZExt(to);
               }
             }
           }
