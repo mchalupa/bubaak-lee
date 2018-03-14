ee30c360ce3772e24006ef6396cba7dc813e8940
Michael Simacek <msimacek@redhat.com>
2018-03-14 23:51:52 +0100

    Avoid manual copying of bytes in realloc

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -4189,22 +4189,18 @@ void Executor::executeAlloc(ExecutionState &state,
       bindLocal(target, state, 
                 KValue(ConstantExpr::alloc(0, Context::get().getPointerWidth())));
     } else {
-      ObjectState *os = bindObjectInState(state, mo, isLocal);
-      if (zeroMemory) {
-        os->initializeToZero();
-      } else {
-        os->initializeToRandom();
-      }
       bindLocal(target, state, mo->getPointer());
-      
-      if (reallocFrom) {
-        unsigned count = std::min(reallocFrom->size, os->size);
-        for (unsigned i=0; i<count; i++)
-          // TODO segment
-          os->write(i, reallocFrom->read8(i));
-        const MemoryObject *reallocObject = reallocFrom->getObject();
-        state.deallocate(reallocObject);
-        state.addressSpace.unbindObject(reallocObject);
+      if (!reallocFrom) {
+        ObjectState *os = bindObjectInState(state, mo, isLocal);
+        if (zeroMemory) {
+          os->initializeToZero();
+        } else {
+          os->initializeToRandom();
+        }
+      } else {
+        ObjectState *os = new ObjectState(*reallocFrom, mo);
+        state.addressSpace.unbindObject(reallocFrom->getObject());
+        state.addressSpace.bindObject(mo, os);
       }
     }
   } else {
