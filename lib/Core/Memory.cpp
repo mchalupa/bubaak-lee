187c20540c26b7d40d06f564161eb47116b6e82e
Michael Simacek <msimacek@redhat.com>
2018-04-18 01:35:13 +0200

    Flush all concrete values on write

--- lib/Core/Memory.cpp
+++ lib/Core/Memory.cpp
@@ -269,6 +269,8 @@ void ObjectStatePlane::flushForWrite() {
       setKnownSymbolic(offset, 0);
     }
   }
+  // everything is potentially overwritten
+  symbolic = true;
 }
 
 bool ObjectStatePlane::isByteConcrete(size_t offset) const {
