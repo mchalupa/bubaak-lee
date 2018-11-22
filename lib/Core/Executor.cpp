551425a6aa6cab4e575bca9be54b2574c6f8497b
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-22 13:08:45 +0100

    Executor: print more information about memory leaks

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3854,11 +3854,31 @@ static bool hasMemoryLeaks(ExecutionState &state) {
   return false;
 }
 
+static std::vector<const MemoryObject *> getMemoryLeaks(ExecutionState &state) {
+    std::vector<const MemoryObject *> leaks;
+    for (auto& object : state.addressSpace.objects) {
+        if (!object.first->isLocal && !object.first->isGlobal
+            && !object.first->isFixed) {
+            // => is heap-allocated
+            leaks.push_back(object.first);
+        }
+    }
+
+    return leaks;
+}
+
 void Executor::terminateStateOnExit(ExecutionState &state) {
   ++stats::terminationExit;
   if (CheckLeaks && hasMemoryLeaks(state)) {
+    auto leaks = getMemoryLeaks(state);
+    if (leaks.empty())
+      return;
+    std::string info;
+    for (const auto *mo : leaks) {
+      info += getAddressInfo(state, mo->getPointer());
+    }
     terminateStateOnProgramError(state, "memory error: memory leak detected",
-                                 StateTerminationType::Leak);
+                                 StateTerminationType::Leak, info);
   } else {
     if (ExitOnErrorType.empty() &&
         (shouldWriteTest(state) || (AlwaysOutputSeeds && seedMap.count(&state))))
