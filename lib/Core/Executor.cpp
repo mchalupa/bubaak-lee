eea42f813a2ba33d2c168712803ffdbe3f61c58a
Marek Chalupa <mchqwerty@gmail.com>
2019-11-25 16:22:02 +0100

    Forbid some external calls
    Calls to functions like fesetround change the environment
    inside KLEE, not inside the analyzed program,
    so we must give up on them.

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -4258,6 +4258,14 @@ static std::set<std::string> okExternals(okExternalsList,
                                          okExternalsList + 
                                          (sizeof(okExternalsList)/sizeof(okExternalsList[0])));
 
+// these are not and may introduce incorrect results.
+// Fail on these if the policy is Pure (for None, the call will fail anyway
+// and for All... well, the user wanted that...)
+static std::set<std::string> nokExternals({"fesetround", "fesetenv",
+                                           "feenableexcept", "fedisableexcept",
+                                           "feupdateenv", "fesetexceptflag",
+                                           "feclearexcept", "feraiseexcept"});
+
 void Executor::callExternalFunction(ExecutionState &state, KInstruction *target,
                                     KCallable *callable,
                                     const std::vector<Cell> &arguments) {
@@ -4268,10 +4276,23 @@ void Executor::callExternalFunction(ExecutionState &state, KInstruction *target,
     return;
 
   if (ExternalCalls == ExternalCallPolicy::Pure &&
-      !okExternals.count(function->getName().str())) {
+      nokExternals.count(callable->getName().str()) > 0) {
+    terminateStateOnUserError(state, "failed external call");
+    return;
+  }
 
-    auto retTy = function->getReturnType();
-    if (retTy->isVoidTy()) {
+  if (ExternalCalls == ExternalCallPolicy::None &&
+      !okExternals.count(callable->getName().str())) {
+    klee_warning("Disallowed call to external function: %s\n",
+                 callable->getName().str().c_str());
+    terminateStateOnUserError(state, "external calls disallowed");
+    return;
+  }
+
+  if (ExternalCalls == ExternalCallPolicy::Pure &&
+      !okExternals.count(callable->getName().str())) {
+    auto *retTy = func->function->getReturnType();
+    if (!func || retTy->isVoidTy()) {
         //klee_warning_once(target, "Skipping call of undefined function: %s",
         //                  function->getName().str().c_str());
         return;
@@ -4283,7 +4304,7 @@ void Executor::callExternalFunction(ExecutionState &state, KInstruction *target,
     auto size = DL.getTypeAllocSizeInBits(retTy);
     if (size > 64) {
         klee_warning_once(target, "Undefined function returns > 64bit object: %s",
-                          function->getName().str().c_str());
+                          callable->getName().str().c_str());
         terminateStateOnUserError(state, "failed external call");
         return;
     }
@@ -4292,14 +4313,14 @@ void Executor::callExternalFunction(ExecutionState &state, KInstruction *target,
     if (retTy->isPointerTy()) {
         isPointer = true;
         klee_warning_once(target, "Returning nondet pointer: %s",
-                         function->getName().str().c_str());
+                          callable->getName().str().c_str());
     }
     auto nv = createNondetValue(state, size, false,
-                                target, function->getName().str(),
+                                target, callable->getName().str(),
                                 isPointer);
     bindLocal(target, state, nv);
     klee_warning_once(target, "Assume that the undefined function %s is pure",
-                      function->getName().str().c_str());
+                      callable->getName().str().c_str());
 
     return;
   }
@@ -5407,7 +5428,7 @@ size_t Executor::getAllocationAlignment(const llvm::Value *allocSite) const {
       allocationSiteName = fn->getName().str();
 
     if (allocationSiteName.compare(0, 17, "__VERIFIER_nondet") == 0) {
-        type = cs.getType();
+        type = cb.getType();
         alignment = 0;
     } else {
       klee_warning_once(fn != NULL ? fn : allocSite,
