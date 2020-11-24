7a61f1df8bac4dd7129f205cbb7d94ef82bf7703
Marek Chalupa <mchqwerty@gmail.com>
2020-11-24 22:18:03 +0100

    Halt the execution on memcleanup error if instructed
    We broke it in one of the previous patches.

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -4261,7 +4261,7 @@ void Executor::terminateStateOnError(ExecutionState &state,
   Instruction * lastInst;
   const InstructionInfo &ii = getLastNonKleeInternalInstruction(state, &lastInst);
 
-  if (CheckMemCleanup) {
+  if (CheckMemCleanup && terminationType != StateTerminationType::Leak) {
     auto leaks = getMemoryLeaks(state);
     if (!leaks.empty()) {
       std::string info = "";
@@ -4274,7 +4274,8 @@ void Executor::terminateStateOnError(ExecutionState &state,
         klee_message("ERROR: %s:%d: %s", ii.file.c_str(), ii.line, message.c_str());
         reportError(message, state, info, suffix, terminationType);
       }
-      return terminateState(state);
+      if (shouldExitOn(StateTerminationType::Leak))
+        haltExecution = true;
     }
   }
 
