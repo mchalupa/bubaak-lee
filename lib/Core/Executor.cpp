5793c5bde9950cf9ca419c988591f12e3db6062f
Marek Chalupa <mchqwerty@gmail.com>
2020-12-02 11:00:41 +0100

    Support setting malloce'd memory to symbolic

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -4790,13 +4790,14 @@ ObjectState *Executor::bindObjectInState(ExecutionState &state,
   return os;
 }
 
-void Executor::executeAlloc(ExecutionState &state,
-                            ref<Expr> size,
-                            bool isLocal,
-                            KInstruction *target,
-                            bool zeroMemory,
-                            const ObjectState *reallocFrom,
-                            size_t allocationAlignment) {
+MemoryObject *
+Executor::executeAlloc(ExecutionState &state,
+                       ref<Expr> size,
+                       bool isLocal,
+                       KInstruction *target,
+                       bool zeroMemory,
+                       const ObjectState *reallocFrom,
+                       size_t allocationAlignment) {
   size = optimizer.optimizeExpr(size, true);
   const llvm::Value *allocSite = state.prevPC->inst;
   if (allocationAlignment == 0) {
@@ -4823,6 +4824,7 @@ void Executor::executeAlloc(ExecutionState &state,
       state.addressSpace.bindObject(mo, os);
     }
   }
+  return mo;
 }
 
 void Executor::executeFree(ExecutionState &state,
