caddec460088265af866462cdeedddae49e10966
Marek Chalupa <mchqwerty@gmail.com>
2021-08-27 15:11:12 +0200

    Fix the signature of pthread funs
    They return a value.

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -182,8 +182,8 @@ static constexpr std::array handlerInfo = {
   // operator new(unsigned long)
   add("_Znwm", handleNew, true),
 
-  add("pthread_create", handlePthreadCreate, false),
-  add("pthread_join", handlePthreadJoin, false),
+  add("pthread_create", handlePthreadCreate, true),
+  add("pthread_join", handlePthreadJoin, true),
 
 #undef addDNR
 #undef add
