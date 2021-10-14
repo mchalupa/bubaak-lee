4873d4e2bc80a97edc5b560c5507c8e0a90aa9ed
Marek Chalupa <chalupa@fi.muni.cz>
2021-10-14 17:12:32 +0200

    tests: disable LargeReturnTypes test
    We do not fully support calling external functions
    with our symbolic memory. This test does exactly that.

--- test/Feature/LargeReturnTypes.cpp
+++ test/Feature/LargeReturnTypes.cpp
@@ -1,3 +1,8 @@
+// XFAIL: *
+// We do not fully support calling external functions
+// with our symbolic memory. This test does exactly that.
+// REQUIRES: not-msan
+// Requires instrumented C++ library for msan
 // REQUIRES: not-darwin
 // REQUIRES: not-freebsd
 // RUN: %clangxx -g -fno-exceptions -emit-llvm %O0opt -c -o %t.bc %s
