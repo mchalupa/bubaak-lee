tareq97/JetKlee

special functions: kill state on unsupported pthread API

lib/Core/SpecialFunctionHandler.cpp
@@ -173,6 +173,9 @@ static SpecialFunctionHandler::HandlerInfo handlerInfo[] = {
 
   add("pthread_create", handlePthreadCreate, true),
   add("pthread_join", handlePthreadJoin, true),
+  add("pthread_key_create", handleUnsupportedPthread, true),
+  add("pthread_set_specific", handleUnsupportedPthread, true),
+  add("pthread_get_specific", handleUnsupportedPthread, true),
 
 #undef addDNR
 #undef add
@@ -1310,3 +1313,11 @@ void SpecialFunctionHandler::handlePthreadJoin(ExecutionState &state,
   executor.terminateStateOnExecError(state,
         "Call to pthread_join.");
 }
+
+void SpecialFunctionHandler::handleUnsupportedPthread(ExecutionState &state,
+                                                      KInstruction *target,
+                                                      const std::vector<Cell> &arguments) {
+  executor.terminateStateOnExecError(state,
+        "unsupported pthread API.");
+}
+

mchalupa/bubaak-lee

lib/Core/SpecialFunctionHandler.cpp
@@ -184,6 +184,9 @@ static constexpr std::array handlerInfo = {
 
   add("pthread_create", handlePthreadCreate, true),
   add("pthread_join", handlePthreadJoin, true),
+  add("pthread_key_create", handleUnsupportedPthread, true),
+  add("pthread_setspecific", handleUnsupportedPthread, true),
+  add("pthread_getspecific", handleUnsupportedPthread, true),
 
 #undef addDNR
 #undef add
@@ -1289,4 +1292,11 @@ void SpecialFunctionHandler::handlePthreadJoin(ExecutionState &state,
                                                const std::vector<Cell> &arguments) {
   executor.terminateStateOnExecError(state,
         "Call to pthread_join.");
+}
+
+void SpecialFunctionHandler::handleUnsupportedPthread(ExecutionState &state,
+                                                      KInstruction *target,
+                                                      const std::vector<Cell> &arguments) {
+  executor.terminateStateOnExecError(state,
+        "unsupported pthread API.");
 }