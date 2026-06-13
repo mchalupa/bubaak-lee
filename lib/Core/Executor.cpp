tareq97/JetKlee

Check memcleanup after abort is called

lib/Core/Executor.cpp
@@ -3398,6 +3398,18 @@ void Executor::terminateStateOnError(ExecutionState &state,
   Instruction * lastInst;
   const InstructionInfo &ii = getLastNonKleeInternalInstruction(state, &lastInst);
 
+  if (termReason == Executor::Abort && CheckMemCleanup) {
+    auto leaks = getMemoryLeaks(state);
+    if (!leaks.empty()) {
+      std::string info = "";
+      for (const auto mo : leaks) {
+        info += getAddressInfo(state, mo->getPointer());
+      }
+      terminateStateOnError(state, "memory error: memory not cleaned up",
+                            Leak, nullptr, info);
+    }
+  }
+
   if (shouldExitOn(termReason))
     haltExecution = true;
 

mchalupa/bubaak-lee

lib/Core/Executor.cpp
@@ -4123,6 +4123,18 @@ void Executor::terminateStateOnError(ExecutionState &state,
   Instruction * lastInst;
   const InstructionInfo &ii = getLastNonKleeInternalInstruction(state, &lastInst);
 
+  if (terminationType == StateTerminationType::Abort && CheckMemCleanup) {
+    auto leaks = getMemoryLeaks(state);
+    if (!leaks.empty()) {
+      std::string info = "";
+      for (const auto *mo : leaks) {
+        info += getAddressInfo(state, mo->getPointer());
+      }
+      terminateStateOnError(state, "memory error: memory not cleaned up",
+                            StateTerminationType::Leak, info);
+    }
+  }
+
   if (shouldExitOn(terminationType))
     haltExecution = true;
 