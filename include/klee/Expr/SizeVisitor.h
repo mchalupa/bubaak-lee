staticafi/JetKlee

Extract SizeVisitor to separate file

include/klee/util/SizeVisitor.h
@@ -1,0 +1,56 @@
+//===-- SizeVisitor.h -------------------------------------------*- C++ -*-===//
+//
+//                     The KLEE Symbolic Virtual Machine
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KLEE_UTIL_SIZE_VISITOR_H
+#define KLEE_UTIL_SIZE_VISITOR_H
+
+#include "klee/Constraints.h"
+#include "klee/Expr.h"
+#include "klee/util/Assignment.h"
+#include "klee/util/ExprVisitor.h"
+
+#include <map>
+
+namespace klee {
+  class SizeVisitor : public ExprVisitor {
+  public:
+    std::map<const Array *, uint64_t> sizes;
+
+    ExprVisitor::Action visitRead(const ReadExpr &expr) {
+      ref<Expr> index = evaluate(expr.index);
+      assert(isa<ConstantExpr>(index) && "index didn't evaluate to a constant");
+      uint64_t &size = sizes[expr.updates.root];
+      size = std::max(size, cast<ConstantExpr>(index)->getZExtValue() + 1);
+      return Action::doChildren();
+    }
+
+    void visitQuery(const Query &query) {
+      for(ConstraintManager::constraint_iterator it = query.constraints.begin();
+        it != query.constraints.end(); ++it){
+        visit(*it);
+      }
+    }
+
+  protected:
+    virtual ref<Expr> evaluate(ref<Expr> expr) = 0;
+  };
+
+  class AssignmentSizeVisitor : public SizeVisitor {
+  private:
+    const Assignment &assignment;
+  public:
+    AssignmentSizeVisitor(const Assignment &assignment) : assignment(assignment) {}
+  protected:
+    ref<Expr> evaluate(ref<Expr> expr) {
+      return assignment.evaluate(expr);
+    }
+  };
+}
+
+#endif

lib/Solver/CexCachingSolver.cpp
@@ -16,6 +16,7 @@
 #include "klee/util/Assignment.h"
 #include "klee/util/ExprUtil.h"
 #include "klee/util/ExprVisitor.h"
+#include "klee/util/SizeVisitor.h"
 #include "klee/Internal/ADT/MapOfSets.h"
 
 #include "klee/SolverStats.h"
@@ -330,29 +331,6 @@ bool CexCachingSolver::computeValue(const Query& query,
   return true;
 }
 
-class SizeVisitor : public ExprVisitor {
-public:
-  std::map<const Array *, uint64_t> sizes;
-  const Assignment &assignment;
-
-  SizeVisitor(const Assignment &assignment) : assignment(assignment) {}
-
-  ExprVisitor::Action visitRead(const ReadExpr &expr) {
-    ref<Expr> index = assignment.evaluate(expr.index);
-    assert(isa<ConstantExpr>(index) && "index didn't evaluate to a constant");
-    uint64_t &size = sizes[expr.updates.root];
-    size = std::max(size, cast<ConstantExpr>(index)->getZExtValue() + 1);
-    return Action::doChildren();
-  }
-
-  void visitQuery(const Query &query) {
-    for(ConstraintManager::constraint_iterator it = query.constraints.begin();
-      it != query.constraints.end(); ++it){
-      visit(*it);
-    }
-  }
-};
-
 bool 
 CexCachingSolver::computeInitialValues(const Query& query,
                                        const std::vector<const Array*> 
@@ -369,7 +347,7 @@ CexCachingSolver::computeInitialValues(const Query& query,
   if (!a)
     return true;
 
-  SizeVisitor sizeVisitor(*a);
+  AssignmentSizeVisitor sizeVisitor(*a);
   sizeVisitor.visitQuery(query);
 
   // FIXME: We should use smarter assignment for result so we don't

mchalupa/bubaak-lee

include/klee/Expr/SizeVisitor.h
@@ -1,0 +1,56 @@
+//===-- SizeVisitor.h -------------------------------------------*- C++ -*-===//
+//
+//                     The KLEE Symbolic Virtual Machine
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KLEE_UTIL_SIZE_VISITOR_H
+#define KLEE_UTIL_SIZE_VISITOR_H
+
+#include "klee/Expr/Assignment.h"
+#include "klee/Expr/Constraints.h"
+#include "klee/Expr/Expr.h"
+#include "klee/Expr/ExprVisitor.h"
+#include "klee/Solver/Solver.h"
+
+#include <map>
+
+namespace klee {
+  class SizeVisitor : public ExprVisitor {
+  public:
+    std::map<const Array *, uint64_t> sizes;
+
+    ExprVisitor::Action visitRead(const ReadExpr &expr) {
+      ref<Expr> index = evaluate(expr.index);
+      assert(isa<ConstantExpr>(index) && "index didn't evaluate to a constant");
+      uint64_t &size = sizes[expr.updates.root];
+      size = std::max(size, cast<ConstantExpr>(index)->getZExtValue() + 1);
+      return Action::doChildren();
+    }
+
+    void visitQuery(const Query &query) {
+      for (const auto &constraint : query.constraints) {
+        visit(constraint);
+      }
+    }
+
+  protected:
+    virtual ref<Expr> evaluate(ref<Expr> expr) = 0;
+  };
+
+  class AssignmentSizeVisitor : public SizeVisitor {
+  private:
+    const Assignment &assignment;
+  public:
+    AssignmentSizeVisitor(const Assignment &assignment) : assignment(assignment) {}
+  protected:
+    ref<Expr> evaluate(ref<Expr> expr) {
+      return assignment.evaluate(expr);
+    }
+  };
+}
+
+#endif

lib/Solver/CexCachingSolver.cpp
@@ -15,6 +15,7 @@
 #include "klee/Expr/Expr.h"
 #include "klee/Expr/ExprUtil.h"
 #include "klee/Expr/ExprVisitor.h"
+#include "klee/Expr/SizeVisitor.h"
 #include "klee/Support/OptionCategories.h"
 #include "klee/Statistics/TimerStatIncrementer.h"
 #include "klee/Solver/SolverImpl.h"
@@ -320,28 +321,6 @@ bool CexCachingSolver::computeValue(const Query& query,
   return true;
 }
 
-class SizeVisitor : public ExprVisitor {
-public:
-  std::map<const Array *, uint64_t> sizes;
-  const Assignment &assignment;
-
-  SizeVisitor(const Assignment &assignment) : assignment(assignment) {}
-
-  ExprVisitor::Action visitRead(const ReadExpr &expr) {
-    ref<Expr> index = assignment.evaluate(expr.index);
-    assert(isa<ConstantExpr>(index) && "index didn't evaluate to a constant");
-    uint64_t &size = sizes[expr.updates.root];
-    size = std::max(size, cast<ConstantExpr>(index)->getZExtValue() + 1);
-    return Action::doChildren();
-  }
-
-  void visitQuery(const Query &query) {
-    for (const auto &constraint : query.constraints) {
-      visit(constraint);
-    }
-  }
-};
-
 bool 
 CexCachingSolver::computeInitialValues(const Query& query,
                                        const std::vector<const Array*> 
@@ -358,7 +337,7 @@ CexCachingSolver::computeInitialValues(const Query& query,
   if (!a)
     return true;
 
-  SizeVisitor sizeVisitor(*a);
+  AssignmentSizeVisitor sizeVisitor(*a);
   sizeVisitor.visitQuery(query);
 
   // FIXME: We should use smarter assignment for result so we don't