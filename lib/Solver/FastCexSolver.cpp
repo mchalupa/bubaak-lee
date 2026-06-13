staticafi/JetKlee

Calculate actual sizes in FastCexSolver

lib/Solver/FastCexSolver.cpp
@@ -16,6 +16,7 @@
 #include "klee/util/ExprEvaluator.h"
 #include "klee/util/ExprRangeEvaluator.h"
 #include "klee/util/ExprVisitor.h"
+#include "klee/util/SizeVisitor.h"
 // FIXME: Use APInt.
 #include "klee/Internal/Support/Debug.h"
 #include "klee/Internal/Support/IntEvaluation.h"
@@ -1085,6 +1086,16 @@ bool FastCexSolver::computeValue(const Query& query, ref<Expr> &result) {
   }
 }
 
+class CexSizeVisitor : public SizeVisitor {
+private:
+  CexData &cd;
+public:
+  CexSizeVisitor(CexData &cd) : cd(cd) {}
+  ref<Expr> evaluate(ref<Expr> expr) {
+    return cd.evaluatePossible(expr);
+  }
+};
+
 bool
 FastCexSolver::computeInitialValues(const Query& query,
                                     const std::vector<const Array*>
@@ -1105,14 +1116,18 @@ FastCexSolver::computeInitialValues(const Query& query,
   if (!hasSolution)
     return true;
 
+  CexSizeVisitor sizeVisitor(cd);
+  sizeVisitor.visitQuery(query);
+
   // Propogation found a satisfying assignment, compute the initial values.
   for (unsigned i = 0; i != objects.size(); ++i) {
     const Array *array = objects[i];
     assert(array);
+    uint64_t size = sizeVisitor.sizes[array];
     std::vector<unsigned char> data;
-    data.reserve(array->size);
+    data.reserve(size);
 
-    for (unsigned i=0; i < array->size; i++) {
+    for (unsigned i=0; i < size; i++) {
       ref<Expr> read = 
         ReadExpr::create(UpdateList(array, 0),
                          ConstantExpr::create(i, array->getDomain()));

mchalupa/bubaak-lee

lib/Solver/FastCexSolver.cpp
@@ -15,7 +15,9 @@
 #include "klee/Expr/ExprEvaluator.h"
 #include "klee/Expr/ExprRangeEvaluator.h"
 #include "klee/Expr/ExprVisitor.h"
+#include "klee/Expr/SizeVisitor.h"
 #include "klee/Solver/IncompleteSolver.h"
+
 #include "klee/Support/Debug.h"
 
 #include "llvm/ADT/APInt.h"
@@ -1096,6 +1098,16 @@ bool FastCexSolver::computeValue(const Query& query, ref<Expr> &result) {
   }
 }
 
+class CexSizeVisitor : public SizeVisitor {
+private:
+  CexData &cd;
+public:
+  CexSizeVisitor(CexData &cd) : cd(cd) {}
+  ref<Expr> evaluate(ref<Expr> expr) {
+    return cd.evaluatePossible(expr);
+  }
+};
+
 bool
 FastCexSolver::computeInitialValues(const Query& query,
                                     const std::vector<const Array*>
@@ -1116,14 +1128,18 @@ FastCexSolver::computeInitialValues(const Query& query,
   if (!hasSolution)
     return true;
 
-  // propagation found a satisfying assignment, compute the initial values.
+  CexSizeVisitor sizeVisitor(cd);
+  sizeVisitor.visitQuery(query);
+
+  // Propogation found a satisfying assignment, compute the initial values.
   for (unsigned i = 0; i != objects.size(); ++i) {
     const Array *array = objects[i];
     assert(array);
+    uint64_t size = sizeVisitor.sizes[array];
     std::vector<unsigned char> data;
-    data.reserve(array->size);
+    data.reserve(size);
 
-    for (unsigned i=0; i < array->size; i++) {
+    for (unsigned i=0; i < size; i++) {
       ref<Expr> read = 
         ReadExpr::create(UpdateList(array, 0),
                          ConstantExpr::create(i, array->getDomain()));